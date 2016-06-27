ae63504729897ff59a63f0ecfd9fb1069bc7ad3b
kaitoii11 <kaitoii1111@gmail.com>
2016-06-27 21:10:11 +0900

    done with UI and handling model

--- src/plugin/SpatialSimulator.java
+++ src/plugin/SpatialSimulator.java
@@ -17,8 +17,7 @@ public class SpatialSimulator {
 	private static SpatialSimulator spatialSimulator;
 	
 	/** The document. */
-	private SBMLDocument document;
-	
+	private SBMLDocument document;	
 	
 	/**
 	 * Gets the single instance of SpatialSimulator.
@@ -60,7 +59,10 @@ public class SpatialSimulator {
 	 * @param argv the argv
 	 */
 	public void simulate(SBMLDocument document, int argc, String[] argv) {
+		System.out.println(document.toSBML());
 		
+		for(int i = 0; i < argv.length; i++)
+			System.out.println(argv[i]);
 	}
 	
 	
