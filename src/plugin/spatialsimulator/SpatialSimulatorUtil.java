073ffdba5c4896fa5c977d66b3e3ed29c9d03eba
kaitoii11 <kaitoii1111@gmail.com>
2016-07-02 02:12:17 +0900

    changed namespaces, updated pom.xml to install with dependencies

---
+++ src/plugin/spatialsimulator/SpatialSimulatorUtil.java
@@ -0,0 +1,19 @@
+package plugin.spatialsimulator;
+// TODO: Auto-generated Javadoc
+
+/**
+ * The Class SpatialSimulatorUtil.
+ *
+ * @author Kaito Ii
+ * 
+ * Date Created: Jun 24, 2016
+ */
+
+public class SpatialSimulatorUtil {
+
+	/** The Constant title. */
+	public static final String title = "Spatial Simulator";
+	
+	
+	public static final String simulatorName = "";
+}
---
+++ src/plugin/spatialsimulator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author Kaito Ii
+ *
+ * Date Created: Jun 29, 2016
+ */
+/**
+ * @author
+ */
+package plugin.spatialsimulator;
