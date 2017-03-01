7615868fe8b0c332ccb1475ebeecf39f9869c9e4
Akira Funahashi <funa@bio.keio.ac.jp>
2017-03-01 23:07:38 +0900

    fixed createjar.sh and pom.xml. now we can import this project to eclipse and build on it.

--- createjar.sh
+++ createjar.sh
@@ -1,6 +1,7 @@
-#! /bin/zsh
+#!/bin/zsh
 
-mvn install:install-file -Dfile=lib/libspatialsimj.jar -DgroupId=spatialsim -DartifactId=libspatialsimj -Dversion=0.0.1 -Dpackaging=jar
+#mvn install:install-file -Dfile=lib/libspatialsimj.jar -DgroupId=spatialsim -DartifactId=libspatialsimj -Dversion=0.0.1 -Dpackaging=jar -DgeneratePom=true
+#mvn install:install-file -Dfile=thirdparty/celldesigner.jar -DgroupId=jp.sbi -DartifactId=celldesigner -Dversion=4.4.0 -Dpackaging=jar -DgeneratePom=true
 mvn install -X
 /bin/cp target/SpatialSimulatorPlugin-0.0.1-SNAPSHOT.jar lib/
 cd lib
